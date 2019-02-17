ce8f1eaf91d6965b4e4fa97c25e6f702d7613ed1
Samantha Yang <samyang@samanthas-macbook-pro-3.local>
2019-02-17 14:36:08 -0800

    Initial Commit

---
+++ blueHouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blueHouse
+//
+//  Created by Samantha Yang on 2/17/19.
+//  Copyright © 2019 theswiftproject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
